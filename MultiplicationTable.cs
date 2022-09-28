ef844c6a47a4e02167038ad3beec94a41b368792
inderocha <114103795+inderocha@users.noreply.github.com>
2022-09-27 23:58:29 -0300

    Create MultiplicationTable.cs

---
+++ MultiplicationTable.cs
@@ -0,0 +1,13 @@
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Tasks; 
+
+//change the number of the 'j' variable to get a result.
+//for example 2,3,4,5,6,7,8,9,10
+
+int j = 2;
+for(int i = 0; i <= 10; i++) 
+{
+    Console.WriteLine($"{j} x {i} = {j * i}");
+}
